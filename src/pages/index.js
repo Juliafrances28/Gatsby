3329cda8d5c095ec0b5f19b3057d7c5ad39061d7
Julia Horan <julia.horan28@gmail.com>
2021-06-04 10:16:27 -0400

    updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,108 +1,127 @@
 import React from "react";
 import Container from "../components/container";
 import Col from "../components/col";
-import Header from "../components/header";
 import Row from "../components/row";
-import Footer from "../components/footer";
+import Header from "../components/header";
+// import ColumnSection from "../components/col";
+// import Footer from "../components/footer";
 import "./style.css";
 
 export default function container() {
   return (
-    <div>
-      <Header />
-      <row>
-        <Col>
+    <div className="grid-rows-7 gap-4">
+      {/* <Header /> */}
+
+      {/* <header className="bg-gray text-white py-6 px-4">
+        <h1 className="text-2xl">Next Departing Trains </h1>
+        <h4> New York City Subway </h4>
+      </header>  */}
+      {/* <Header />
+      <Container>
+        <Row>
           {" "}
-          <h2 class="up"> Uptown</h2>{" "}
-        </Col>
-        <Col>
-          {" "}
-          <h2 class="down"> Downtown </h2>{" "}
-        </Col>
-      </row>
+          <div>
+            <div>
+              {" "}
+              <h2 className="grid-rows-1">
+                {" "}
+                Uptown &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; &emsp;
+                &emsp;&emsp; &emsp; Downtown{" "}
+              </h2>{" "}
+            </div>
+          </div>
+        </Row>
+      </Container> */}
 
       <Container>
         <Row>
           <Col>
-            <div class="left-col">
-              <div class="row">
-                <div class="station"> to Eastchester-Dyre Av </div>
-                <div class="time">1 min</div>
+            <div className="left-col md 6">
+              <div className="grid grid-rows-7 grid-flow-col-2 gap-4">
+                <div className="station"> to Eastchester-Dyre Av </div>
+                <div className="time"> 1 min </div>
               </div>
 
-              <div class="row">
-                <div class="station">to Woodlawn</div>
-                <div class="time">2 min </div>
+              <div className="grid grid-rows-7 grid-flow-col-2 gap-4">
+                <div className="station">to Woodlawn </div>
+                <div className="time"> 2 min </div>
               </div>
 
-              <div class="row">
-                <div class="station">to Harlem-148 St</div>
-                <div class="time"> 5 min </div>
+              <div className="grid grid-rows-7 grid-flow-col-2 gap-4">
+                <div className="station">to Harlem-148 St </div>
+                <div className="time"> 5 min </div>
               </div>
 
-              <div class="row">
-                <div class="station">to Wakefield 241 St </div>
-                <div class="time"> 8 min </div>
+              <div className=" grid grid-rows-7 grid-flow-col-2 gap-4">
+                <div className="station">to Wakefield 241 St </div>
+                <div className="time"> 8 min </div>
               </div>
 
-              <div class="row">
-                <div class="station">to Jamaica Center- Parson/Archer Av </div>
-                <div class="time">10 min </div>
+              <div className=" grid grid-rows-7 grid-flow-col-2 gap-4">
+                <div className="station">
+                  to Jamaica Center- Parson/Archer Av{" "}
+                </div>
+                <div className="time"> 10 min </div>
               </div>
 
-              <div class="row">
-                <div class="station">to Far Rockaway - Motts Av </div>
-                <div class="time">10 min </div>
+              <div className=" grid grid-rows-7 grid-flow-col-2 gap-4">
+                <div className="station">to Far Rockaway - Motts Av </div>
+                <div className="time"> 10 min </div>
               </div>
 
-              <div class="row">
-                <div class="station"> to Ozone Park-Lefferts </div>
-                <div class="time"> 15 min</div>
+              <div className=" grid grid-rows-7 grid-flow-col-2 gap-4">
+                <div className="station"> to Ozone Park-Lefferts </div>
+                <div className="time"> 15 min </div>
               </div>
             </div>
           </Col>
         </Row>
+      </Container>
+      <Container>
         <Row>
           <Col>
-            <div class="right-col">
-              <div class="row">
-                <div class="station"> to Broad St </div>
-                <div class="time"> 1 min </div>
+            <div className="right-col- md 6">
+              <div className=" grid grid-rows-7 grid-col-2 gap-4">
+                <div className="station"> to Broad St </div>
+                <div className="time"> 1 min </div>
               </div>
 
-              <div class="row">
-                <div class="station"> to Euclid Av </div>
-                <div class="time"> 3 min </div>
+              <div className=" grid grid-rows-7 grid-flow-col-2 gap-4">
+                <div className="station"> to Euclid Av </div>
+                <div className="time"> 3 min </div>
               </div>
 
-              <div class="row">
-                <div class="station"> to Crown Hts-Utica Av </div>
-                <div class="time"> 5 min </div>
+              <div className=" grid grid-rows-7 grid-flow-col-2 gap-4">
+                <div className="station"> to Crown Hts-Utica Av </div>
+                <div className="time"> 5 min </div>
               </div>
 
-              <div class="row">
-                <div class="station"> to New Lots Av </div>
-                <div class="time"> 7 min</div>
+              <div className=" grid grid-rows-7 grid-flow-col-2 gap-4">
+                <div className="station"> to New Lots Av </div>
+                <div className="time"> 7 min</div>
               </div>
 
-              <div class="row">
-                <div class="station"> to Flatbush Av- Brooklyn College </div>
-                <div class="time"> 9 min</div>
+              <div className=" grid grid-rows-7 grid-flow-col-2 gap-4">
+                <div className="station">
+                  {" "}
+                  to Flatbush Av- Brooklyn College{" "}
+                </div>
+                <div className="time"> 9 min</div>
               </div>
 
-              <div class="row">
-                <div class="station">to Crown Hts-Utica Av </div>
-                <div class="time"> 10 min </div>
+              <div className=" grid grid-rows-7 grid-flow-col-2 gap-4">
+                <div className="station">to Crown Hts-Utica Av </div>
+                <div className="time"> 10 min </div>
               </div>
 
-              <div class="row">
-                <div class="station">to Broad St </div>
-                <div class="time"> 20 min </div>
+              <div className=" grid grid-rows-7 grid-flow-col-2 gap-4">
+                <div className="station">to Broad St </div>
+                <div className="time"> 20 min </div>
               </div>
             </div>
           </Col>
         </Row>
-        <Footer />
+        {/* <Footer /> */}
       </Container>
     </div>
   );
---
+++ src/pages/index2.js
@@ -0,0 +1,44 @@
+import { Container } from "postcss";
+import React from "react";
+import ColumnSection from "../components/col";
+import Row from "../components/row";
+
+<Container>
+  <Row>
+    <ColumnSection>
+      <div className="flex mb-4">
+        <div class="w-full bg grey h-12"> happy </div>
+      </div>
+      {/* <div class="h-64 grid grid-rows-3 grid-col-2 gap-4">
+        <div>to woodlawn</div>
+        <div>2 min </div>
+      </div>
+      ;
+      <div class="h-64 grid grid-rows-3 grid-flow-col gap-4">
+        <div>to woodlawn</div>
+        <div>2 min </div>
+      </div>
+      ;
+      <div class="h-64 grid grid-rows-3 grid-flow-col gap-4">
+        <div>to woodlawn</div>
+        <div>2 min </div>
+      </div>
+      ;
+      <div class="h-64 grid grid-rows-3 grid-flow-col gap-4">
+        <div>to woodlawn</div>
+        <div>2 min </div>
+      </div>
+      ;
+      <div class="h-64 grid grid-rows-3 grid-flow-col gap-4">
+        <div>to woodlawn</div>
+        <div>2 min </div>
+      </div>
+      ;
+      <div class="h-64 grid grid-rows-3 grid-flow-col gap-4">
+        <div>to woodlawn</div>
+        <div>2 min </div>
+      </div>
+      ; */}
+    </ColumnSection>
+  </Row>
+</Container>;
