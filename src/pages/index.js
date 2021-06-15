f35a6235e5efb82ca22b0d7cca06c33cdffd3cce
Julia Frances <julia.horan28@gmail.com>
2021-06-15 13:29:09 -0400

    columns line up

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,50 +13,50 @@ export default function container() {
   return (
     <div>
       <Header />
-      <Container className="horizontal">
+      <Container className="horizontal grid">
         <Row>
           <Col>
-            <div className="left-col md 6">
-              <div className="grid">
-                <div className="station">
+            <div className="left-col md 6 ">
+              <div className="grid auto ">
+                <div className="Up">
                   {" "}
                   <strong> Uptown </strong>{" "}
                 </div>
               </div>
 
-              <div className="grid">
+              <div className="grid auto ">
                 <div className="station">to Eastchester-Dyre Av </div>
-                <div className="time"> 1 min </div>
+                <div className="time place-content-between"> 1 min </div>
               </div>
 
-              <div className="grid">
+              <div className="grid auto ">
                 <div className="station">to Woodlawn </div>
                 <div className="time"> 2 min </div>
               </div>
 
-              <div className="grid">
+              <div className="grid auto">
                 <div className="station">to Harlem-148 St </div>
                 <div className="time"> 5 min </div>
               </div>
 
-              <div className=" grid">
+              <div className=" grid auto">
                 <div className="station">to Wakefield 241 St </div>
                 <div className="time"> 8 min </div>
               </div>
 
-              <div className=" grid">
+              <div className=" grid auto">
                 <div className="station">
                   to Jamaica Center- Parson/Archer Av
                 </div>
                 <div className="time"> 10 min </div>
               </div>
 
-              <div className=" grid">
+              <div className=" grid auto">
                 <div className="station">to Far Rockaway - Motts Av </div>
                 <div className="time"> 10 min </div>
               </div>
 
-              <div className=" grid">
+              <div className=" grid auto">
                 <div className="station"> to Ozone Park-Lefferts </div>
                 <div className="time"> 15 min </div>
               </div>
@@ -66,35 +66,35 @@ export default function container() {
 
         <Row>
           <Col>
-            <div className="right-col md 6">
-              <div className="grid">
-                <div className="station">
+            <div className="right-col md 6  ">
+              <div className="grid auto">
+                <div className="Down">
                   {" "}
                   <strong> Downtown </strong>{" "}
                 </div>
               </div>
 
-              <div className=" grid">
+              <div className=" grid auto">
                 <div className="station"> to Broad St </div>
-                <div className="time"> 1 min </div>
+                <div className="time flex"> 1 min </div>
               </div>
 
-              <div className=" grid">
+              <div className=" grid auto">
                 <div className="station"> to Euclid Av </div>
                 <div className="time"> 3 min </div>
               </div>
 
-              <div className=" grid">
+              <div className=" grid auto">
                 <div className="station"> to Crown Hts-Utica Av </div>
                 <div className="time"> 5 min </div>
               </div>
 
-              <div className=" grid">
+              <div className=" grid auto">
                 <div className="station"> to New Lots Av </div>
                 <div className="time"> 7 min</div>
               </div>
 
-              <div className=" grid grid-cols">
+              <div className=" grid auto">
                 <div className="station">
                   {" "}
                   to Flatbush Av- Brooklyn College{" "}
@@ -102,12 +102,12 @@ export default function container() {
                 <div className="time"> 9 min</div>
               </div>
 
-              <div className=" grid">
+              <div className=" grid auto">
                 <div className="station">to Crown Hts-Utica Av </div>
                 <div className="time"> 10 min </div>
               </div>
 
-              <div className=" grid ">
+              <div className=" grid auto ">
                 <div className="station">to Broad St </div>
                 <div className="time"> 20 min </div>
               </div>
